cface024ba621c00248dc8a7521b475f14475198
Franciuto <francesco.traversetolo@gmail.com>
2025-09-29 12:53:23 +0200

    simple exercise

---
+++ simple.java
@@ -0,0 +1,12 @@
+public class aura {
+    public static void main(String[] args) {
+        int n = 3;
+        if (n < 0) {
+            System.out.print("La variabile " + n + " è negativa");
+        } else if (n == 0) {
+            System.out.print("La variabile è uguale a zero");
+        } else {
+            System.out.print("La variabile " + n + " è positiva");
+        }
+    }
+}
