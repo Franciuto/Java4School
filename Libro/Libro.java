a690242746d8b7785dc4b530d1a3b25b184a223f
Franciuto <francesco.traverstolo@gmail.com>
2025-10-13 11:00:14 +0200

    Libro

---
+++ Libro/Libro.java
@@ -0,0 +1,55 @@
+public class Libro {
+    private String title;
+    private String author;
+    private int pageN;
+    private double pageCost;
+    final private double FIXED_COST = 2.4;
+
+    Libro(String title, String author, int pageN) {
+        this.title = title;
+        this.author = author;
+        this.pageN = pageN;
+    }
+    
+    // Getter + Setter
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPageN() {
+        return pageN;
+    }
+
+    public void setPageN(int pageN) {
+        this.pageN = pageN;
+    }
+
+    public void setPageCost (double pageCost) {
+        this.pageCost = pageCost;
+    }
+
+    public double prezzo() {
+        return pageN * pageCost + FIXED_COST;
+    }   
+
+    public static void main(String[] args) {
+        Libro l1 = new Libro("Harry Potter", "J.K. Rowling", 830);
+        Libro l2 = new Libro("The Hobbit", "J.R.R. Tolkien", 310);
+
+        l1.setPageCost(0.02);
+        l2.setPageCost(0.03);
+        System.out.printf("%.2f€", l1.prezzo());
+    }
+}
