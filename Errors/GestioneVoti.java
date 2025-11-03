797d6a4d0bec4e563902a171a4178efd2b9c8258
Franciuto <francesco.traversetolo@gmail.com>
2025-11-03 12:38:28 +0100

    error managing

---
+++ Errors/GestioneVoti.java
@@ -0,0 +1,32 @@
+public class GestioneVoti {
+    private static int sommaVoti = 0;
+    private static int votiValidi = 0;
+    String[] voti = {"8", "10", "abc", "12", "-3", "7"};
+
+    public static void main(String[] args) {
+        GestioneVoti g = new GestioneVoti();
+
+        for (String s : g.voti) {
+            try {
+                int voto = Integer.parseInt(s);
+                if (voto >= 0 && voto <= 10) {
+                    sommaVoti += voto;
+                    votiValidi++;
+                } else {
+                    // voto numerico ma fuori dal range 0-10
+                    System.out.println("Voto fuori range: " + s);
+                }
+            } catch (NumberFormatException e) {
+                // stringa non convertibile in intero
+                System.out.println("Voto non valido: " + s);
+            }
+        }
+
+        if (votiValidi > 0) {
+            double media = (double) sommaVoti / votiValidi;
+            System.out.printf("Media dei voti validi: %.2f%n", media);
+        } else {
+            System.out.println("Nessun voto valido.");
+        }
+    }
+}
